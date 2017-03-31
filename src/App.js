4e3611c3c9e0909df7555df4441c91274e89131d
Tim Macdonald <macdonald.tim@gmail.com>
2017-03-30 20:57:01 -0400

    Added basic redux functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const { message, changeMessage } = this.props
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>{message}</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={changeMessage}>Change message</button>
         </p>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  message: state.message
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  changeMessage: () => {
+    dispatch({ type: 'CHANGE_MESSAGE', message: 'This is a new message' })
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 
+const reducer = (store = { message: "Hello, React" } , action) => {
+  if (action.type == 'CHANGE_MESSAGE') {
+    return { message: action.message }
+  }
+  return store
+}
+
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
